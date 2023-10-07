7104d9d12e8efa38e66046b9e4ae2b1202b7c7f8
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-08 00:17:10 +0100

    add profile page + styling.

--- routes/pages.js
+++ routes/pages.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/profile', (req, res) => {
+    res.render('profile');
+});
+
 router.get('/register', (req, res) => {
     res.render('register');
 });
