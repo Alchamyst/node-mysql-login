1dc1cc99b16ee1875bde9f876deef508f4cdcb9c
Alchamyst <ashley@ashley-yetman.co.uk>
2023-10-07 14:37:13 +0100

    style home page. add new page for user registration.

--- app.js
+++ app.js
@@ -29,8 +29,11 @@ database.connect( (error) => {
 });
 
 app.get("/", (req, res) => {
-    //res.send("<h1> Home Page</h1>");
     res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
 });
 
 app.listen(9000, () => {
